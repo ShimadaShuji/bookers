d3724fe7d0966ad0ab70de18c98ce65790436274
ShujiShimada <cocoa.s.simada@gmail.com>
2021-08-23 12:11:13 +0000

    [finish]3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+
+  def index
+    @books=Book.all
+    @book =Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "メッセージの送信に成功しました"
+      redirect_to book_path(@book.id)
+    else
+      flash[:danger] ="メッセージを入力してください"
+      render:new
+    end
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book =Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+ def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to books_path
+ end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
